20693a86f119850569593bc54295c07268bead5c
Jérémie Perret <jeremie.perret@bluewin.ch>
2023-07-20 13:49:08 +0200

    home banner design

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -5,7 +5,28 @@ const Accueil = () => {
     return (
         <>
             <Menu />
-            <article className=""></article>
+            <article className="banner-article">
+                <p>
+                    EnVRAC est une structure qui vise à ouvrir des opportunités dans la production
+                    audiovisuelle en suisse romande.
+                </p>
+                <p>
+                    Cette structure est fondée sur des valeurs d’entraide et de coopération entre
+                    indépendants passionnés.
+                </p>
+                <p>
+                    Le désir d’avoir des conditions de travail professionnelles et confortables, ainsi
+                    qu’une relation à l’écoute des clients sont primordiaux.
+                </p>
+                <p>
+                    Quel que soit le domaine d’exécution, EnVRAC se tient à fournir une prestation
+                    optimisée pour sa clientèle.
+                </p>
+                <p>
+                    Promouvoir la scène locale en accompagnant les néophytes comme les plus
+                    expérimentés est, sans discernement, une valeur fondamentale.
+                </p>
+            </article>
             <Footer />
         </>
     )
