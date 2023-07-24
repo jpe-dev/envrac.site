243c27712baeac3315d6e288c57c7174a04bc1b7
Jérémie Perret <jeremie.perret@bluewin.ch>
2023-07-24 11:50:38 +0200

    Correcting warnings

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -16,14 +16,14 @@ const Presentation = () => {
                 <p className="w-40">Basé à Echallens, EnVRAC studio est spécialisé dans l’enregistrement de voix et le mixage professionnel.</p>
                 <p className="w-40">Le studio est composé d’un Control room principal et d’une petite Guest room.</p>
                 <p className="w-40">La polyvalence qu’offre les locaux permet de proposer différents services annexes, axés autour de la production musicale. (Voix off, Interview, Shooting photo, Artwork, etc.)</p>
-                <img className="w-50 right" src={studioImg} alt="image of studio" />
+                <img className="w-50 right" src={studioImg} alt="studio" />
             </div>
             <div className="content m-t-1">
                 <h1 id="work-space">le work-space</h1>
                 <p className="w-40">
                     Un espace de Co-working équipé et dédié à la création visuelle, nous permet de proposer différents services annexes, axés autour de la production musicale. (voix off, interview, émissions, shooting photo, fond vert, artwork, etc.)
                 </p>
-                <img className="w-50 right" src={workSpaceImg} alt="image of studio" />
+                <img className="w-50 right" src={workSpaceImg} alt="work-space" />
 
             </div>
             <Footer />
