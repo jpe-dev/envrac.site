f215e6be7bf9cf74c8a13f231d4228b1ce498320
Jérémie Perret <jeremie.perret@bluewin.ch>
2023-07-26 12:26:46 +0200

    no ad-block anymore

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -5,8 +5,6 @@ import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 const Footer = () => {
     return (
         <View id='footer-wrap'>
-            {/* TODO Barre d'annonce tournante */}
-            <div className='a123456789'><div className='ad-text'>Sit do sunt enim anim officia aute magna tempor. Sit quis veniam enim commodo fugiat. Laboris labore qui mollit laborum sunt culpa dolore pariatur fugiat occaecat voluptate et reprehenderit consequat. Incididunt ex adipisicing consequat ullamco reprehenderit id amet nisi est est esse ea. Laboris sunt officia in aliqua deserunt reprehenderit elit duis do do enim excepteur excepteur exercitation. Officia occaecat incididunt aliqua et ipsum ex et consequat aliquip quis aliqua ea ut minim.</div></div>
             <footer>
                 <p>EnVRAC Copyright 2023</p>
                 <p><a href='/cookiepolicy'>Cookie Policy</a>-<a href='/legalterms'>Legal Terms</a>-<a href='/privacypolicy'>Privacy Policy</a></p>
