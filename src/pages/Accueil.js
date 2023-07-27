95ec17311f52665a18d82dc0e499e3f0b8458a2c
jpe-dev <jeremie.perret@bluewin.ch>
2023-07-27 16:45:03 +0200

    changed home design

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,38 +1,36 @@
-import { useEffect } from "react"
-import Footer from "./Footer"
-import Menu from "./Menu"
-import { useTranslation } from "react-i18next"
-
+import { useEffect } from "react";
+import Footer from "./Footer";
+import Menu from "./Menu";
+import { useTranslation } from "react-i18next";
+import envracBanner from "../images/banner.jpeg";
 
 const Accueil = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    useEffect(() => {
-        document.body.classList.add('background');
-    }, []);
-    return (
-        <>
-            <Menu />
-            <article className="banner-article">
-                <p>
-                    {t('accueil_1')}
-                </p>
-                <p>
-                    {t('accueil_2')}
-                </p>
-                <p>
-                    {t('accueil_3')}
-                </p>
-                <p>
-                    {t('accueil_4')}
-                </p>
-                <p>
-                    {t('accueil_5')}
-                </p>
-            </article>
-            <Footer />
-        </>
-    )
-}
+  useEffect(() => {
+    document.body.classList.remove("background");
+  }, []);
+  return (
+    <>
+      <Menu />
+      <div className='w-50 left justify-center h-40'>
+        <img
+          src={envracBanner}
+          alt='envrac banner'
+          className='w-80 p-t-2_5p'
+          style={{ opacity: 0.2 }}
+        />
+      </div>
+      <article className='banner-article right w-50'>
+        <p>{t("accueil_1_2")}</p>
+        <p>{t("accueil_2_2")}</p>
+        <p>{t("accueil_3_2")}</p>
+        <p>{t("accueil_4_2")}</p>
+        <p>{t("accueil_5_2")}</p>
+      </article>
+      <Footer />
+    </>
+  );
+};
 
-export default Accueil
+export default Accueil;
