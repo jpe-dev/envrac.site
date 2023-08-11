2a7229c50c18180f09b420eb97b117564d2d8d22
Jérémie Perret <jeremie.perret@bluewin.ch>
2023-08-11 14:22:24 +0200

    change text for presentation

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -17,13 +17,15 @@ const Presentation = () => {
                 <h1 id="studio">{t('menu_presentation_1')}</h1>
                 <p className="w-40">{t('pres_studio_1')}</p>
                 <p className="w-40">{t('pres_studio_2')}</p>
-                <p className="w-40">{t('pres_studio_3')}</p>
                 <img className="w-50 right" src={studioImg} alt="studio" />
             </div>
             <div className="content m-t-1 m-b-1">
                 <h1 id="work-space">{t('menu_presentation_2')}</h1>
                 <p className="w-40">
                     {t('pres_work_1')}
+                </p>
+                <p className="w-40">
+                    {t('pres_work_2')}
                 </p>
                 <img className="w-50 right" src={workSpaceImg} alt="work-space" />
 
