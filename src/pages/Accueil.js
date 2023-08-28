ee44ccfc851c1ebb1c5437e494062f3abe0ca611
Jérémie Perret <jeremie.perret@bluewin.ch>
2023-08-28 13:22:30 +0200

    home design ajust

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -13,21 +13,30 @@ const Accueil = () => {
     return (
         <>
             <Menu />
-            <div className='w-50 left justify-center h-40'>
-                <img
-                    src={envracBanner}
-                    alt='envrac banner'
-                    className='w-80 p-t-2_5p'
-                    style={{ opacity: 0.5 }}
-                />
+            <div>
+                <div className='w-50 justify-center h-40'>
+                    <img
+                        src={envracBanner}
+                        alt='envrac banner'
+                        className='w-80 p-t-2_5p'
+                        style={{ opacity: 0.5 }}
+                    />
+                </div>
+                <div className='banner-article w-50 right'>
+                    <p>{t("accueil_1_2")}</p>
+                    <p>{t("accueil_2_2")}</p>
+                    <p>{t("accueil_3_2")}</p>
+                </div>
             </div>
-            <article className='banner-article right w-50'>
-                <p>{t("accueil_1_2")}</p>
-                <p>{t("accueil_2_2")}</p>
-                <p>{t("accueil_3_2")}</p>
-                <p>{t("accueil_4_2")}</p>
-                <p>{t("accueil_5_2")}</p>
-            </article>
+            <div className="w-100 justify-center" style={{ marginTop: '50%' }}>
+                <div className="w-80 centered-content">
+                    <p>{t("accueil_1_4")}</p>
+                    <p>{t("accueil_2_4")}</p>
+                    <p>{t("accueil_3_4")}</p>
+                    <p>{t("accueil_4_4")}</p>
+                    <p>{t("accueil_5_4")}</p>
+                </div>
+            </div>
             <Footer />
         </>
     );
