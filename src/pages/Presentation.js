99a60b2d4e082b40ee900b73a72428f9725cda1b
Jérémie Perret <jeremie.perret@bluewin.ch>
2023-08-29 11:14:09 +0200

    presentation page design and text ajust

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -15,19 +15,23 @@ const Presentation = () => {
             <Menu />
             <div className="content">
                 <h1 id="studio">{t('menu_presentation_1')}</h1>
-                <p className="w-40">{t('pres_studio_1')}</p>
-                <p className="w-40">{t('pres_studio_2')}</p>
-                <img className="w-50 right" src={studioImg} alt="studio" />
+                <div className="flex-row space-between">
+                    <div className="flex-col w-40">
+                        <p>{t('pres_studio_1')}</p>
+                        <p>{t('pres_studio_2')}</p>
+                    </div>
+                    <img className="w-50" src={studioImg} alt="studio" />
+                </div>
             </div>
             <div className="content m-t-1 m-b-1">
                 <h1 id="work-space">{t('menu_presentation_2')}</h1>
-                <p className="w-40">
-                    {t('pres_work_1')}
-                </p>
-                <p className="w-40">
-                    {t('pres_work_2')}
-                </p>
-                <img className="w-50 right" src={workSpaceImg} alt="work-space" />
+                <div className="flex-row space-between">
+                    <div className="flex-col w-40">
+                        <p>{t('pres_work_1')}</p>
+                        <p>{t('pres_work_2')}</p>
+                    </div>
+                    <img className="w-50" src={workSpaceImg} alt="work-space" />
+                </div>
 
             </div>
             <div className="content m-t-1 m-b-1">
