dcea076d54e86af80a1373d80d0922cf26d289c5
Jérémie Perret <jeremie.perret@bluewin.ch>
2023-08-30 22:51:47 +0200

    cours todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Accueil, Booking, Contact, Docs, Portfolio, Presentation, Services } from "./pages"
+import { Accueil, Booking, Contact, Docs, Portfolio, Presentation, Services, /*Cours*/ } from "./pages"
 
 import './App.css';
 
