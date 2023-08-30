21947cf43123493ef42ae9044c402897c6082787
Jérémie Perret <jeremie.perret@bluewin.ch>
2023-08-30 23:14:56 +0200

    association presentation paragraph and stuff

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -22,8 +22,9 @@ const Presentation = () => {
                     </div>
                     <img className="w-50" src={studioImg} alt="studio" />
                 </div>
+                <hr className="m-t-3"></hr>
             </div>
-            <div className="content m-t-1 m-b-1">
+            <div className="content m-t-2 m-b-1">
                 <h1 id="work-space">{t('menu_presentation_2')}</h1>
                 <div className="flex-row space-between">
                     <div className="flex-col w-40">
@@ -32,31 +33,33 @@ const Presentation = () => {
                     </div>
                     <img className="w-50" src={workSpaceImg} alt="work-space" />
                 </div>
-
+                <hr className="m-t-3"></hr>
             </div>
-            <div className="content m-t-1 m-b-1">
+            <div className="content m-t-2 m-b-1">
                 <h1 id="equipement">{t('menu_presentation_5')}</h1>
-                <p className="w-100">
-                    {t('pres_equ_1')}
-                </p>
-                <p className="w-80">
-                    {t('pres_equ_2')}
-                </p>
-                <p className="w-80">
-                    {t('pres_equ_3')}
-                </p>
-                <p className="w-80">
-                    {t('pres_equ_4')}
-                </p>
-                <p className="w-80">
-                    {t('pres_equ_5')}
-                </p>
-                <p className="w-80">
-                    {t('pres_equ_6')}
-                </p>
-                <p className="w-80">
-                    {t('pres_equ_7')}
-                </p>
+                <div className="w-80">
+                    <p>{t('pres_equ_1')}</p>
+                    <p>{t('pres_equ_2')}</p>
+                    <p>{t('pres_equ_3')}</p>
+                    <p>{t('pres_equ_4')}</p>
+                    <p>{t('pres_equ_5')}</p>
+                    <p>{t('pres_equ_6')}</p>
+                    <p>{t('pres_equ_7')}</p>
+                </div>
+                <hr className="m-t-3"></hr>
+            </div>
+            <div className="content m-t-2 m-b-1">
+                <h1 id="association">{t('menu_presentation_4')}</h1>
+                <p>{t('presentation_association_1')}</p>
+                <p>{t('presentation_association_2')}</p>
+                <br />
+                <br />
+                <p>{t('presentation_association_3')}</p>
+                <p>{t('presentation_association_4')}</p>
+                <p>{t('presentation_association_5')}</p>
+                <p>{t('presentation_association_6')}</p>
+                <p>{t('presentation_association_7')}</p>
+                <p>{t('presentation_association_8')}</p>
             </div>
             <Footer />
         </>
