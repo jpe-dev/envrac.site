e1914d92a031a8b2c7d458aa84d16e4f0182b9cc
Jérémie Perret <jeremie.perret@bluewin.ch>
2023-09-07 11:59:16 +0200

    adapting iframe size

--- src/components/Accordion/booking.js
+++ src/components/Accordion/booking.js
@@ -5,10 +5,8 @@ export const accordionData = [
         content: (
             <iframe
                 src='https://koalendar.com/e/forfait-essentiel?embed=true'
-                width='100%'
-                height='875px'
-                frameborder='0'
                 title='essentiel'
+                frameborder='0'
             ></iframe>
         ),
     },
@@ -18,10 +16,8 @@ export const accordionData = [
         content: (
             <iframe
                 src='https://koalendar.com/e/forfait-avance?embed=true '
-                width='100%'
-                height='875px'
-                frameborder='0'
                 title='avance'
+                frameborder='0'
             ></iframe>
         ),
     },
@@ -31,10 +27,8 @@ export const accordionData = [
         content: (
             <iframe
                 src='https://koalendar.com/e/forfait-deluxe?embed=true '
-                width='100%'
-                height='875px'
-                frameborder='0'
                 title='deluxe'
+                frameborder='0'
             ></iframe>
         ),
     },
@@ -44,8 +38,6 @@ export const accordionData = [
         content: (
             <iframe
                 src='https://koalendar.com/e/forfait-maestro?embed=true '
-                width='100%'
-                height='925px'
                 frameborder='0'
                 title='maestro'
             ></iframe>
