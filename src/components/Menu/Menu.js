b909dfa99d0131dbf7f94fd12c7ff4de080248b3
Jérémie Perret <jeremie.perret@bluewin.ch>
2023-09-10 18:56:00 +0200

    add smooth burger animation

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -120,12 +120,17 @@ const Menu = () => {
                 </ul>
             </nav>
             <div className='navigationHamburger'>
-                <div className='hamburger' onClick={toggleHamburger}>
-                    <div className='burger'></div>
-                    <div className='burger'></div>
-                    <div className='burger'></div>
-                </div>
-                <ul className='menuHamburger'>
+                <input
+                    type='checkbox'
+                    role='button'
+                    aria-label='Display the menu'
+                    class='menuToggle'
+                    onClick={toggleHamburger}
+                />
+                <ul className={`menuHamburger ${hamburgerOpen ? "open" : ""}`}>
+                    <li>
+                        <Link to='/'>{t("menu_accueil")}</Link>
+                    </li>
                     <li>
                         <Link to='/presentation'>{t("menu_presentation")}</Link>
                         <div className='subnavHamburger'>
@@ -218,8 +223,21 @@ const Menu = () => {
                 </ul>
             </div>
             <style jsx>{`
-                .navigationHamburger ul {
-                    display: ${hamburgerOpen ? "inline" : "none"};
+                .menuHamburger {
+                    height: 0;
+                    visibility: hidden;
+                    opacity: 0;
+                    overflow: hidden;
+                    transition-delay: 1s;
+                    transition: visibility 0.5s linear, opacity 0.5s linear,
+                        height 0.5s linear;
+                }
+                .menuHamburger.open {
+                    height: auto;
+                    visibility: visible;
+                    list-style-type: none;
+                    transition-delay: 0.25s;
+                    opacity: 1;
                 }
                 .navigationHamburger li {
                     padding: 0.25rem;
@@ -232,26 +250,83 @@ const Menu = () => {
                     flex-direction: column;
                     margin: 1rem 3rem;
                 }
-                .hamburger {
-                    width: 2rem;
-                    height: 2rem;
+                .menuToggle {
                     display: none;
-                    justify-content: space-around;
-                    flex-flow: column nowrap;
-                    padding: 0.25rem;
+                    margin: 1rem -2rem;
+                    --s: 100px; /* control the size */
+                    --c: #ceb9d5; /* the color */
+
+                    height: var(--s);
+                    aspect-ratio: 1;
+                    border: none;
+                    padding: 0;
+                    border-inline: calc(var(--s) / 2) solid #0000;
+                    box-sizing: content-box;
+                    --_g1: linear-gradient(
+                            var(--c) 20%,
+                            #0000 0 80%,
+                            var(--c) 0
+                        )
+                        no-repeat content-box border-box;
+                    --_g2: radial-gradient(
+                            circle closest-side at 50% 12.5%,
+                            var(--c) 95%,
+                            #0000
+                        )
+                        repeat-y content-box border-box;
+                    background: var(--_g2) left var(--_p, 0px) top,
+                        var(--_g1) left calc(var(--s) / 10 + var(--_p, 0px)) top,
+                        var(--_g2) right var(--_p, 0px) top,
+                        var(--_g1) right calc(var(--s) / 10 + var(--_p, 0px))
+                            top;
+                    background-size: 20% 80%, 40% 100%;
+                    position: relative;
+                    clip-path: inset(0 25%);
+                    -webkit-mask: linear-gradient(
+                        90deg,
+                        #0000,
+                        #000 25% 75%,
+                        #0000
+                    );
+                    cursor: pointer;
+                    transition: background-position 0.3s var(--_s, 0.3s),
+                        clip-path 0s var(--_s, 0.6s);
+                    -webkit-appearance: none;
+                    -moz-appearance: none;
+                    appearance: none;
                 }
-                .burger {
-                    width: 2rem;
-                    height: 0.25rem;
-                    border-radius: 10px;
-                    background-color: #ceb9d5;
-                    transform-origin: 1px;
-                    transition: all 0.3s linear;
+                .menuToggle:before,
+                .menuToggle:after {
+                    content: "";
+                    position: absolute;
+                    border-radius: var(--s);
+                    inset: 40% 0;
+                    background: var(--c);
+                    transition: transform 0.3s calc(0.3s - var(--_s, 0.3s));
+                }
+
+                .menuToggle:checked {
+                    clip-path: inset(0);
+                    --_p: calc(-1 * var(--s));
+                    --_s: 0s;
+                }
+                .menuToggle:checked:before {
+                    transform: rotate(45deg);
+                }
+                .menuToggle:checked:after {
+                    transform: rotate(-45deg);
+                }
+                .menuToggle:focus-visible {
+                    clip-path: none;
+                    -webkit-mask: none;
+                    border: none;
+                    outline: 2px solid var(--c);
+                    outline-offset: 5px;
                 }
                 @media (orientation: portrait) {
                     @media (max-width: 1080px) {
-                        .hamburger {
-                            display: flex;
+                        .menuToggle {
+                            display: block;
                         }
                         nav {
                             display: none;
